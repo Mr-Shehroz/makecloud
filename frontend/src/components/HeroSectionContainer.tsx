d3e99992cbe5d93a8c46b4bcea657f50e0d46e1e
Mr-Shehroz <mr.shehroz.khan2006@gmail.com>
2026-02-13 12:43:41 +0500

    Refactor hero data fetching in HeroSectionContainer
    Removed comments and unused fields from hero data fetch.

--- frontend/src/components/HeroSectionContainer.tsx
+++ frontend/src/components/HeroSectionContainer.tsx
@@ -4,22 +4,15 @@ import HeroSection from './HeroSection'
 import type { HeroData } from '@/types/hero'
 
 export default async function HeroSectionContainer() {
-  // Fetch hero data from Sanity
   const heroData: HeroData | null = await client.fetch(
     `*[_type == "hero"][0]{
       _id,
       _type,
-      logos[] {
-        _key,
-        alt,
-        logo,
-        order
-      } | order(order asc),
+      partnersLogo,
       heading,
       subheading,
       ctaButton {
         text,
-        cloudLogo,
         url,
         openInNewTab
       }
