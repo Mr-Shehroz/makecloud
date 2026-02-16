5ce86b3e1f017d1046934617a22c30dad8d1094a
Mr-Shehroz <shehrozkhan.wow@gmail.com>
2026-02-16 16:56:00 +0500

    feat: add teh sanity route

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -6,8 +6,8 @@ import { codeInput } from "@sanity/code-input";
 import { unsplashImageAsset } from "sanity-plugin-asset-source-unsplash";
 import { structure } from './structure';
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_STUDIO_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_STUDIO_DATASET || "production";
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineConfig({ 
 	name: "default",
